f3cbb17cab986f537f129bb74f0e2ec535121688
oleg <o1eg.34ac@gmail.com>
2022-04-17 22:30:47 +0300

    Add transaction model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :categories
+  resources :transactions
   resources :users
   resources :wallets
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_17_175325) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_17_191914) do
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
@@ -18,6 +18,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_17_175325) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer "amount"
+    t.string "note"
+    t.integer "transaction_type"
+    t.bigint "wallet_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["wallet_id"], name: "index_transactions_on_wallet_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,5 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_17_175325) do
     t.index ["user_id"], name: "index_wallets_on_user_id"
   end
 
+  add_foreign_key "transactions", "wallets"
   add_foreign_key "wallets", "users"
 end
