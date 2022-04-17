1587f5745c9001a5d1b042da17155f4acd64f8e4
oleg <o1eg.34ac@gmail.com>
2022-04-17 23:03:10 +0300

    Remove useless migrations

--- db/migrate/20220416201029_add_kind_to_categories.rb
+++
@@ -1,5 +0,0 @@
-class AddKindToCategories < ActiveRecord::Migration[7.0]
-  def change
-    add_column :categories, :kind, :integer
-  end
-end
--- db/migrate/20220417181904_remove_kind_from_categories.rb
+++
@@ -1,5 +0,0 @@
-class RemoveKindFromCategories < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :categories, :kind, :integer
-  end
-end
