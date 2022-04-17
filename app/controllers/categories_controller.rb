d644c28b7bf7b96fa341ecef92b69640922fafe6
oleg <o1eg.34ac@gmail.com>
2022-04-17 23:07:37 +0300

    Remove kind field

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -57,15 +57,14 @@ class CategoriesController < ApplicationController
     end
   end
 
-
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_category
-      @category = Category.find(params[:id])
-    end
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def category_params
-      params.require(:category).permit(:title, :image, :kind)
-    end
+  # Only allow a list of trusted parameters through.
+  def category_params
+    params.require(:category).permit(:title, :image)
+  end
 end
--- app/views/categories/_category.json.jbuilder
+++ app/views/categories/_category.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! category, :id, :title, :image, :kind, :created_at, :updated_at
+json.extract! category, :id, :title, :image, :created_at, :updated_at
 json.url category_url(category, format: :json)
