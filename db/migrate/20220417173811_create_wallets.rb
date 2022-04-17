3a4a05eb0b0f82abb47019d10f3b19d6f1985ba2
oleg <o1eg.34ac@gmail.com>
2022-04-17 20:48:58 +0300

    Add wallet model

---
+++ db/migrate/20220417173811_create_wallets.rb
@@ -0,0 +1,13 @@
+class CreateWallets < ActiveRecord::Migration[7.0]
+  def change
+    create_table :wallets do |t|
+      t.text :name
+      t.text :currency
+      t.integer :quantity
+
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -31,4 +31,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_17_175325) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  create_table "wallets", force: :cascade do |t|
+    t.text "name"
+    t.text "currency"
+    t.integer "quantity"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_wallets_on_user_id"
+  end
+
+  add_foreign_key "wallets", "users"
 end
