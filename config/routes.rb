734174502b7e9290133893bbdf70fda1be080fde
Yulia Lime <lime666lime@gmail.com>
2022-04-19 22:24:29 +0300

    devise-views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  #delete 'users/sign_out', to: 'devise/sessions#destroy'
 
   resources :categories
   resources :transactions
   resources :users
   resources :wallets
+
 end
