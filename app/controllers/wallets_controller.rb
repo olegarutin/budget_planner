d64208201789ecbeca1d5d5c9f21bb0d14a2927e
oleg <o1eg.34ac@gmail.com>
2022-04-19 18:21:45 +0300

    Create a wallet controller and page to create it

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,2 +1,22 @@
 class WalletsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @wallet = Wallet.create(wallet_params)
+    redirect_to root_path
+  end
+
+  def update
+    @wallet.update(wallet_params)
+  end
+
+  def destroy
+    @wallet.destroy
+  end
+
+  private
+
+  def wallet_params
+    params.permit(:name, :currency, :quantity, :user_id)
+  end
 end
