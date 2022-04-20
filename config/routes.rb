5943ea4a39746ba0f37c79af648175eb5ac2379f
oleg <o1eg.34ac@gmail.com>
2022-04-20 12:16:24 +0300

    Fix log_out button

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  #delete 'users/sign_out', to: 'devise/sessions#destroy'
 
   resources :categories
   resources :transactions
   resources :users
   resources :wallets
-
 end
