41803b92f1436b592990d41625edfe6d9a62a23a
oleg <o1eg.34ac@gmail.com>
2022-04-26 17:48:00 +0300

    Add column transaction_type to categories table

--- db/migrate/20220416185851_create_categories.rb
+++ db/migrate/20220416185851_create_categories.rb
@@ -3,6 +3,7 @@ class CreateCategories < ActiveRecord::Migration[7.0]
     create_table :categories do |t|
       t.string :title
       t.string :image
+      t.integer :transaction_type
 
       t.timestamps
     end
