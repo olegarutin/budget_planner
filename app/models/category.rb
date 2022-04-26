9730173cfabf77cda8a8b0b8b32c16e90b805b48
olegarutin <77899931+olegarutin@users.noreply.github.com>
2022-04-26 20:43:35 +0300

    6 add default categories (#6)
    * Add column transaction_type to categories table
    
    * Use enum
    
    * Add default categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ApplicationRecord
   validates :title, presence: true
 
-  belongs_to :user
+  belongs_to :user, optional: true
   has_many :transactions, dependent: :destroy
+
+  enum transaction_type: %i[income expense]
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,5 +2,5 @@ class Transaction < ApplicationRecord
   belongs_to :wallet
   belongs_to :category
 
-  enum transaction_type: ['expense', 'income']
+  enum transaction_type: %i[income expense]
 end
