cbba4486cc8dfb13aac5498ed0756ddb17b4214d
lime666 <lime666lime@gmail.com>
2022-04-27 22:58:41 +0300

    7 transaction view (#7)
    * transaction's crud
    
    * transaction create fixes
    
    * Fix codestyle
    
    * Remove useless file
    
    * Undo changes
    
    * Improve transaction page
    
    Co-authored-by: oleg <o1eg.34ac@gmail.com>

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,26 @@
 class TransactionsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @transactions = Transaction.all
+  end
+
+  def create
+    @transaction = Transaction.create(transaction_params)
+    if @transaction.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @transaction.update(transaction_params)
+  end
+
+  private
+
+  def transaction_params
+    params.permit(:amount, :note, :transaction_type, :wallet_id, :category_id)
+  end
 end
