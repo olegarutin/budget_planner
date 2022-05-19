03bbb065604fdcf770ac8b5bdc0885122e893145
oleg <o1eg.34ac@gmail.com>
2022-05-19 15:48:36 +0300

    Add validation for transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -10,6 +10,8 @@ class Transaction < ApplicationRecord
 
   enum transaction_type: TYPES
 
+  validates :amount, numericality: { greater_than: 0 }
+
   after_create_commit { broadcast_append_to 'transactions' }
   after_update_commit { broadcast_replace_to 'transactions' }
   after_destroy_commit { broadcast_remove_to 'transactions' }
