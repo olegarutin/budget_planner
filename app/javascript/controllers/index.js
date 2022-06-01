3540be25e6fb255d3a984e841a08103a1fb50df5
oleg <o1eg.34ac@gmail.com>
2022-06-01 20:10:31 +0300

    Connect stimulus-password-visibility controllers

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,8 @@
 import { application } from './application';
 import controllers from './**/*_controller.js';
+import PasswordVisibility from 'stimulus-password-visibility';
+
+application.register('password-visibility', PasswordVisibility);
 
 controllers.forEach((controller) => {
   application.register(controller.name, controller.module.default);
