7a90501733c6b6fd57e83ddb1908a22a524693fe
oleg <o1eg.34ac@gmail.com>
2022-06-05 14:38:50 +0300

    Clear cache if user logs out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :turbo_frame_request_variant
+  before_action :set_cache_buster
 
   private
 
@@ -9,5 +10,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     dashboards_path
+  end
+
+  def set_cache_buster
+    response.headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
   end
 end
