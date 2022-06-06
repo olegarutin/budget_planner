265bceb18c4eab82fed03cf8c27e5a5651d81d9b
Yulia Lime <lime666lime@gmail.com>
2022-06-06 23:07:06 +0300

    sort by date without default

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,8 @@
 class ReportsController < ApplicationController
   def index
     # @expense_by_days = Wallet.date_range(created_at.day, Date.today).quantity
-    #@users_transactions_by_days = current_user.transactions.for_date_range(created_at.day..Date.today)
-    @transaction_type = params[:transaction_type]
-    @start_date = params[:start_date]
-    @end_date = params[:end_date]
+    @transaction_type = params[:transaction_type] || 'expense'
+    @start_date = params[:start_date] || 7.days.ago.to_date.to_s
+    @end_date = params[:end_date] || Date.today.to_date.to_s
   end
 end
--- app/models/concerns/transactionable.rb
+++ app/models/concerns/transactionable.rb
@@ -1,23 +1,23 @@
 module Transactionable
   extend ActiveSupport::Concern
 
-  def user_categories(transaction_type)
-    transactions.public_send(transaction_type)
+  def user_categories(transaction_type, start_date, end_date)
+    transactions.for_date_range(start_date, end_date)
+      .public_send(transaction_type)
       .pluck(:category_id).uniq
   end
 
-  def amount_for_categories(transaction_type)
-    transactions.public_send(transaction_type)
+  def amount_for_categories(transaction_type, start_date, end_date)
+    transactions.for_date_range(start_date, end_date)
+      .public_send(transaction_type)
       .includes(:category).group(:category_id)
       .sum(:amount).values
   end
 
-  def amount_for_income(type: :income)
-    transactions.income.includes(:category).group(:category_id).sum(:amount).values
-  end
-
-  def category_title(type: :expense)
-    transactions.public_send(type).includes(:category).pluck(:title).uniq
+  def category_title(transaction_type, start_date, end_date)
+    transactions.for_date_range(@start_date, @end_date)
+      .public_send(transaction_type)
+      .includes(:category).pluck(:title).uniq
   end
 
   def all_categories_titles
@@ -30,9 +30,5 @@ module Transactionable
 
   def expense_categories_title
     transactions.expense.includes(:category).pluck(:title).uniq
-  end
-
-  def transactoinable_params
-    #params[:transaction_type], params[:start_date], params[:end_date]
   end
 end
