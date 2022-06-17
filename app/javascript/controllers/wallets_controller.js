21283de6d550de16ad07449e14156c6bb138eff0
oleg <o1eg.34ac@gmail.com>
2022-06-17 16:15:44 +0300

    Reset wallet create form after closing modal window

--- app/javascript/controllers/wallets_controller.js
+++ app/javascript/controllers/wallets_controller.js
@@ -3,6 +3,7 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
   resetModal() {
     $('#wallet_errors').empty();
+    $('#AddWallet').find('form').trigger('reset');
   }
 
   hideModal(e) {
