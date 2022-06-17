47b21dc9e905dc0482f900314aea4d83b82876ce
oleg <o1eg.34ac@gmail.com>
2022-06-17 13:24:32 +0300

    Connect inputmask for number and password fields

--- app/javascript/controllers/inputmask_controller.js
+++ app/javascript/controllers/inputmask_controller.js
@@ -1,10 +1,19 @@
 import { Controller } from '@hotwired/stimulus';
-import Inputmask from "inputmask";
+import Inputmask from 'inputmask';
 
 export default class extends Controller {
   static targets = ['field']
+  static values = { type: String }
 
   connect() {
-    // Inputmask("99999").mask(this.fieldTarget);
+    Inputmask({ placeholder: '', regex: this.regex }).mask(this.fieldTarget);
+  }
+
+  get regex() {
+    if (this.typeValue == 'password') {
+      return '^[A-Za-z0-9]+$';
+    } else {
+      return '^[0-9]*[.|,]?[0-9]{2}$';
+    }
   }
 }
