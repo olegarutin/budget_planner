4ab792dd09e0e18a77a4734dc2b5377ccd9473f0
oleg <o1eg.34ac@gmail.com>
2022-06-18 15:54:31 +0300

    Allow float number for wallet

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -9,4 +9,9 @@ class Wallet < ApplicationRecord
   scope :date_range, ->(start_date, end_date) { where(created_at: start_date.beginning_of_day..end_date.end_of_day) }
 
   after_create_commit { broadcast_append_to 'wallets' }
+  after_update_commit { broadcast_replace_to 'wallets' }
+
+  def convert_to_currency_format
+    quantity / 100.to_f
+  end
 end
