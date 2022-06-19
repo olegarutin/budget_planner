8b0d0e31d3d33a14071bb5abcf803f1bdfa544ad
oleg <o1eg.34ac@gmail.com>
2022-06-19 20:57:24 +0300

    Fix mistake

--- app/javascript/controllers/wallets_controller.js
+++ app/javascript/controllers/wallets_controller.js
@@ -1,6 +1,8 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
+  static targets = ['wallet']
+
   resetModal() {
     $('#wallet_errors').empty();
     $('#AddWallet').find('form').trigger('reset');
