e03a37d769c7044f3c7c199432776f6723e546cc
Yulia_Lime <lime666lime@gmail.com>
2022-06-23 16:19:58 +0300

    added-select-wallet-to-charts

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,16 +1,8 @@
 class ReportsController < ApplicationController
   def index
-    #debugger
-    # set_wallet
-    @user_wallet = params[:user_wallet]
+    @user_wallet = params[:user_wallet] || current_user.wallets.first
     @transaction_type = params[:transaction_type] || 'expense'
     @start_date = params[:start_date] || 7.days.ago.to_date.to_s
     @end_date = params[:end_date] || Date.tomorrow.to_date.to_s
   end
-
-  private
-
-  # def set_wallet
-  #   @user_wallet = Wallet.find(params[:])
-  # end
 end
--- app/javascript/download_pdf.js
+++ app/javascript/download_pdf.js
@@ -2,10 +2,10 @@ $(document).on("click","#download-pdf",function() {
   alert("Do you want to download charts in pdf format?");
     var charts_pdf = document.getElementById('create-charts-pdf');
     var opt = {
-      margin:       1,
-      filename:     $("#report-from").data("date"),
-      image:        { type: 'jpeg', quality: 0.98 },
-      html2canvas:  { scale: 2 },
-      jsPDF:        { unit: 'in', format: 'tabloid', orientation: 'landscape' }};
+      margin: 1,
+      filename: $("#report-from").data("date"),
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'in', format: 'tabloid', orientation: 'landscape' }};
     html2pdf().set(opt).from(charts_pdf).save();
 });
