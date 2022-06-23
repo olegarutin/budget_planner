95681710bd94f9b4bee7bd95c30301069eaa2008
oleg <o1eg.34ac@gmail.com>
2022-06-23 18:54:39 +0300

    Fix turbo wallet update

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -8,6 +8,7 @@ class Wallet < ApplicationRecord
 
   scope :date_range, ->(start_date, end_date) { where(created_at: start_date.beginning_of_day..end_date.end_of_day) }
 
+  # after_create_commit { broadcast_prepend_to 'wallets' }
   after_destroy_commit { broadcast_remove_to 'wallets' }
 
   def convert_to_currency_format
