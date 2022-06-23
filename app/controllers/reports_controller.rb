9664a273b9d9227f184a68b199c405009fb5f616
Yulia_Lime <lime666lime@gmail.com>
2022-06-23 16:09:15 +0300

    download-button-for-pdf

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,16 @@
 class ReportsController < ApplicationController
   def index
+    #debugger
+    # set_wallet
     @user_wallet = params[:user_wallet]
     @transaction_type = params[:transaction_type] || 'expense'
     @start_date = params[:start_date] || 7.days.ago.to_date.to_s
     @end_date = params[:end_date] || Date.tomorrow.to_date.to_s
   end
+
+  private
+
+  # def set_wallet
+  #   @user_wallet = Wallet.find(params[:])
+  # end
 end
