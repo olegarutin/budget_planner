fa8fa7583a91f5603cfae9c53a8d40fa6bac3fe6
Yulia Lime <lime666lime@gmail.com>
2022-06-26 23:11:06 +0300

    responsive-display-and-dobload-pdf

--- app/javascript/download_pdf.js
+++ app/javascript/download_pdf.js
@@ -5,7 +5,24 @@ $(document).on("click","#download-pdf",function() {
       margin: 1,
       filename: $("#report-from").data("date"),
       image: { type: 'jpeg', quality: 0.98 },
-      html2canvas: { scale: 2 },
+      html2canvas: { scale: 2,
+                     width: 1500,
+                     height: 900
+                   },
       jsPDF: { unit: 'in', format: 'tabloid', orientation: 'landscape' }};
+    html2pdf().set(opt).from(charts_pdf).save();
+});
+
+$(document).on("click","#download-pdf-mobile",function() {
+  alert("Do you want to download charts in pdf format?");
+    var charts_pdf = document.getElementById('create-charts-pdf');
+    var opt = {
+      margin: 0,
+      filename: $("#report-from").data("date"),
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2,
+                     windowWidth: 792
+                   },
+      jsPDF: { unit: 'in', format: 'letter', orientation: 'l' }};
     html2pdf().set(opt).from(charts_pdf).save();
 });
