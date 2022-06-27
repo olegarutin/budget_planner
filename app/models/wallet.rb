f8c07fcba6b18ac2e303406d0812cc5a45734f7b
oleg <o1eg.34ac@gmail.com>
2022-06-27 20:32:58 +0300

    Fix max value for models

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -11,7 +11,7 @@ class Wallet < ApplicationRecord
 
   validates :name, uniqueness: { scope: :user_id, case_sensitive: false }, length: { maximum: 20 }, on: :create
   validates :name, :currency, presence: true
-  validates :quantity, numericality: { greater_than_or_equal_to: 0, less_than: 1_000_000_00 }, on: :create
+  validates :quantity, numericality: { greater_than_or_equal_to: 0, less_than: 100_000_000_000 }, on: :create
 
   scope :date_range, ->(start_date, end_date) { where(created_at: start_date.beginning_of_day..end_date.end_of_day) }
 
