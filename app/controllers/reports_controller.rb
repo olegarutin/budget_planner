fbc15a8c10c7f498e3157ea2facca067c6787079
Yulia Lime <lime666lime@gmail.com>
2022-06-30 00:28:52 +0300

    small-report-date-fixes

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,8 +2,8 @@ class ReportsController < ApplicationController
   def index
     @user_wallet = params[:user_wallet] || current_user.wallets.last
     @transaction_type = params[:transaction_type] || 'expense'
-    @start_date = params[:start_date] || (Time.now.to_date - 7.days)
-    @end_date = params[:end_date] || Time.now.to_date
+    @start_date = params[:start_date] || (Time.current.to_date - 7.days)
+    @end_date = params[:end_date] || Time.current.to_date
     @previous_wallet = params[:user_wallet]
     @previous_type = params[:transaction_type]
   end
