0d06726fbad6b3afe8d122a1884d2d73b1e12264
Yulia Lime <lime666lime@gmail.com>
2022-06-29 20:06:52 +0300

    fixed-date-fields-on-reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,7 @@ class ReportsController < ApplicationController
   def index
     @user_wallet = params[:user_wallet] || current_user.wallets.first
     @transaction_type = params[:transaction_type] || 'expense'
-    @start_date = params[:start_date] || 7.days.ago.to_date.to_s
-    @end_date = params[:end_date] || DateTime.current.to_date.to_s
+    @start_date = params[:start_date] || (Time.now.to_date - 7.days)
+    @end_date = params[:end_date] || Time.now.to_date
   end
 end
--- config/initializers/service_worker.rb
+++
@@ -1,6 +0,0 @@
-# Rails.application.configure do
-#   config.serviceworker.routes.draw do
-#     get "/service_worker.js", to: "dashboards#index"
-#     get "/manifest.json", to: "dashboards#index"
-#   end
-# end
