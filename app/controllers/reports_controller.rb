a8b6360aea5cf34c2ed60db54d47b73d2f864adb
Yulia Lime <lime666lime@gmail.com>
2022-06-29 23:53:27 +0300

    fixed-capitalize-transaction-type

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
   def index
-    @user_wallet = params[:user_wallet] || current_user.wallets.first
+    @user_wallet = params[:user_wallet] || current_user.wallets.last
     @transaction_type = params[:transaction_type] || 'expense'
     @start_date = params[:start_date] || (Time.now.to_date - 7.days)
     @end_date = params[:end_date] || Time.now.to_date
